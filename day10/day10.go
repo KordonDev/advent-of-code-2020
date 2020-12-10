cf5f991abcddcd4e0dcd742c35fa0974466dacff
Arne Maier <Arne_Maier@gmx.de>
2020-12-10 21:11:44 +0100

    Fast version for day 10

--- day10/day10.go
+++ day10/day10.go
@@ -13,7 +13,6 @@ func main() {
 		numbers = append(numbers, stringToInt(line))
 	}
 	sort.Ints(numbers)
-	fmt.Println(numbers)
 
 	threeJoltsJumps := 1 // last jump
 	oneJoltsJumps := 0
@@ -31,7 +30,48 @@ func main() {
 	}
 	fmt.Println("result one", oneJoltsJumps*threeJoltsJumps)
 
-	cutIndex := 64
+	possibilities := 1
+	for index, currentJolt := range numbers {
+		possibileJolts := 0
+		for _, nextJolt := range numbers[index+1 : rangeEnd(index, &numbers)] {
+			if nextJolt-currentJolt <= 3 {
+				possibileJolts++
+			}
+		}
+		if possibileJolts != 0 {
+			possibilities = possibilities * possibileJolts
+		}
+	}
+	fmt.Println("Wrong Part2 without recursion", possibilities)
+
+	sum := make(chan int)
+	sliceStart := 0
+	sliceStartJolt := 0
+	slices := 1
+	for index, currentJolt := range numbers {
+		if index != len(numbers)-1 && numbers[index+1]-currentJolt == 3 {
+			subSlice := numbers[sliceStart : index+1]
+			go calculateSlicePosibilities(sliceStartJolt, &subSlice, sum)
+			sliceStart = index + 1
+			sliceStartJolt = currentJolt
+			slices++
+		}
+	}
+	subSlice := numbers[sliceStart:]
+	go calculateSlicePosibilities(sliceStartJolt, &subSlice, sum)
+
+	result := 1
+	for i := 0; i < slices; i++ {
+		next := <-sum
+		result = result * next
+	}
+	fmt.Println("Part 2 fast", result)
+
+	// part2Old(numbers)
+}
+
+func part2Old(numbers []int) {
+	cutIndex := 64 // only works for this input data
 
 	possibilitiesFirstHalf := 0
 	firstHalf := numbers[0:cutIndex]
@@ -41,8 +81,6 @@ func main() {
 	secondHalf := numbers[cutIndex:]
 	addNextCharger(numbers[cutIndex-1], -1, &secondHalf, &possibilitiesSecondHalf)
 
-	fmt.Println(firstHalf, secondHalf)
-	fmt.Println(possibilitiesFirstHalf, "*", possibilitiesSecondHalf)
 	fmt.Println("Second result", possibilitiesSecondHalf*possibilitiesFirstHalf)
 
 }
@@ -64,4 +102,10 @@ func rangeEnd(currentIndex int, adapterJolts *[]int) int {
 		return currentIndex + 4
 	}
 	return len(*adapterJolts)
+}
+
+func calculateSlicePosibilities(currentJolts int, adapterJolts *[]int, res chan int) {
+	possibilities := 0
+	addNextCharger(currentJolts, -1, adapterJolts, &possibilities)
+	res <- possibilities
 }
